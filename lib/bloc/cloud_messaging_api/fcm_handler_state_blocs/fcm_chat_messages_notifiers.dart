93c678c8325899118e07cd1fe6663b5d1cde65d5
andrua-joshua <andruajoshua096@gmail.com>
2023-07-17 18:24:54 +0300

    here with some few changes to test

--- lib/bloc/cloud_messaging_api/fcm_handler_state_blocs/fcm_chat_messages_notifiers.dart
+++ lib/bloc/cloud_messaging_api/fcm_handler_state_blocs/fcm_chat_messages_notifiers.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
-//import 'package:path';
 import 'package:propup/bloc/cloud_messaging_api/fcm_models/fcm_chat_message_model.dart';
-import 'package:propup/bloc/json_modules/chats_message_json_model.dart';
 
 //ignore:camel_case_types
 class fcmChatMessagesNotifiers with ChangeNotifier {
@@ -19,19 +18,66 @@ class fcmChatMessagesNotifiers with ChangeNotifier {
   Map<String, List<chatMessage>> allChatMessages() => _allChatMessages;
 
   void addChatMessage({required chatMessage message}) {
-    (message.recieverID == FirebaseAuth.instance.currentUser?.uid)
-        ? _allChatMessages.putIfAbsent(message.senderId, () => [message])
-        : _allChatMessages.putIfAbsent(message.recieverID, () => [message]);
+     (message.recieverID == FirebaseAuth.instance.currentUser?.uid)
+    ?_allChatMessages.putIfAbsent(message.senderId, () => [message])
+    : _allChatMessages.putIfAbsent(message.recieverID, () => [message]);
 
+    serializeChatsToJson();
     notifyListeners();
   }
 
-  //for seializing and saving the chats to the local storage
-  void serializeChatsToJson() async{
+  Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
 
-    //jsonEncode(_allChatMessages);
+  return directory.path;
+}
 
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/drilloxhts.json');
+  }
+
+  //for serializing and saving the chats to the local storage
+  void serializeChatsToJson() async {
+    List<Map<String, dynamic>> allchts = [];
+
+    _allChatMessages.forEach((key, value) {
+      List<Map<String, dynamic>> chats = [];
+
+      for (var element in value) {
+        chats.add(element.toJson());
+      }
+
+      allchts.add({"chatId": key, "chats": chats});
+    });
+
+    //this being the string to be saved
+    String jsonEncoded = jsonEncode(allchts);
+
+    final file = await _localFile;
+    await file.writeAsString(jsonEncoded);
+
+  }
+
+  void deserializeFromJson() async{
     
-    
+    final file = await _localFile;
+
+    final source  = await file.readAsString();
+
+
+    List<Map<String, dynamic>> allChats =
+        jsonDecode(source) as List<Map<String, dynamic>>;
+
+    for (Map<String, dynamic> element in allChats) {
+      List<chatMessage> chats = [];
+
+      for (var value in element['chats'] as List<Map<String, dynamic>>) {
+        chats.add(chatMessage.fromJson(json: value));
+      }
+
+      _allChatMessages.addAll({element['chatId'] as String, chats}
+          as Map<String, List<chatMessage>>);
+    }
   }
 }
--- lib/routes/messaging_screen.dart
+++ lib/routes/messaging_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:propup/bloc/cloud_messaging_api/fcm_handler_state_blocs/fcm_chat_messages_notifiers.dart';
 import 'package:propup/widgets/messaging_screen_widgets.dart';
+import 'package:provider/provider.dart';
 
 ///
 ///the messaging screen
@@ -10,19 +12,43 @@ class messagingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String chatId = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
       appBar: AppBar(
-        leadingWidth: 20,
-          title: const messagingTopWidget(
-              name: "Nicolus mark", image: "assets/images/pp2.jpg")),
-
-              body:const SafeArea(
-                child: Column(
-                  children: [
-                    Expanded(child:Center(child: Text("no message yet", style: TextStyle(color: Colors.grey),),),),
-                    Padding(padding: EdgeInsets.all(10) , child:sendingWidget())
-                  ],
-                )),
+          leadingWidth: 20,
+          title: messagingTopWidget(
+            chatId: chatId,
+          )),
+      body: SafeArea(
+          child: Column(
+        children: [
+          ChangeNotifierProvider<fcmChatMessagesNotifiers>(
+            create: (context) => fcmChatMessagesNotifiers(),
+            builder: (context, child) {
+              return Expanded(child: SingleChildScrollView(
+                child: Consumer<fcmChatMessagesNotifiers>(
+                    builder: (context, value, child) {
+                  return Column(
+                    children: List.generate(
+                        fcmChatMessagesNotifiers()
+                            .allChatMessages()[chatId]!
+                            .length,
+                        (index) => messageWidget(
+                            chat: fcmChatMessagesNotifiers()
+                                .allChatMessages()[chatId]![index])),
+                  );
+                }),
+              ));
+            },
+          ),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: sendingWidget(
+                chatId: chatId,
+              ))
+        ],
+      )),
     );
   }
 }
